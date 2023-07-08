5c188df7471a887c77f5ef6dc6a49bb713f3fa6a
叶玉国 <yeyuguo@bytedance.com>
2023-07-08 16:18:28 +0800

    feat: 🎸 运行成功

--- packages/demo/src/app.tsx
+++ packages/demo/src/app.tsx
@@ -1,21 +1,49 @@
 import { LiveProvider, LiveEditor, LivePreview, LiveError } from "react-live";
 
-const code = `
-function handleClick() {
-  console.log("Hi there!");
-}
-const HelloWorld = () => (
-  <button onClick={handleClick}>
-    Hello World
-  </button>
-);
-render(<HelloWorld />);
-`.trim();
+import styled from 'styled-components';
 
-export const DemoApp = () => {
+import * as Arco from "@arco-design/web-react"
+import "@arco-design/web-react/dist/css/arco.css";
+
+
+// 公共作用域
+const scope = {
+  ...Arco,
+  styled,
+};
+
+// const code = `
+// function handleClick() {
+//   console.log("Hi there!");
+// }
+// const HelloWorld = () => (
+//   <button onClick={handleClick}>
+//     Hello World
+//   </button>
+// );
+// render(<HelloWorld />);
+// `.trim();
+
+interface ILiveProps {
+  code: string,
+  myScope?: Record<string, any>
+}
+
+export const DemoApp = ({ code, myScope }: ILiveProps) => {
+  // const renderCodeString = `render(${code})`
+  const renderScope = {
+    ...scope,
+    ...myScope
+  }
+  console.log("🚀 ~ file: app.tsx:38 ~ DemoApp ~ renderScope:", renderScope)
   return (
     <div>
-      <LiveProvider code={code} noInline>
+      <LiveProvider code={code} scope={renderScope} noInline
+        transformCode={(codeString: string) => {
+          console.log("🚀 ~ file: app.tsx:43 ~ DemoApp ~ codeString:", codeString)
+          return codeString
+        }}
+      >
         <div className="grid lg:grid-cols-2 gap-4">
           <LiveEditor className="font-mono" />
           <LivePreview />
